d6f44d0040902b12f0d0c7a791ea2915ee19109d
Nahuel <nahuel.profesor@gmail.com>
2023-02-07 17:35:37 -0300

    Guardando el nuevo password del usuario

--- src/paginas/NuevoPassword.jsx
+++ src/paginas/NuevoPassword.jsx
@@ -1,12 +1,14 @@
 import { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 import Alerta from "../components/Alerta";
 import clienteAxios from "../config/axios";
 
 const NuevoPassword = () => {
   const [password, setPassword] = useState("");
   const [alerta, setAlerta] = useState({});
-  const [tokenValido, setTokenValido] = useState(false)
+  const [tokenValido, setTokenValido] = useState(false);
+  const [passwordModificado, setPasswordModificado] = useState(false); 
+
   const params = useParams();
   const { token } = params;
 
@@ -28,6 +30,31 @@ const NuevoPassword = () => {
     comprobarToken();
   }, []);
 
+  const handleSubmit = async (e) => {
+    e.preventDeault()
+
+    if(password.length < 6){
+      setAlerta({
+        msg: 'EL password debe ser cómo mínimo de 6 caracteres',
+        error: true
+      })
+    }
+
+    try {
+      const url = `/veterinarios/olvide-password/${token}`
+      const { data } = await clienteAxios.post(url, {password})
+      setAlerta({
+        msg: data.msg
+      }) 
+      setPasswordModificado(true);
+    } catch (error) {
+      setAlerta({
+        msg: error.response.data.msg,
+        error: true
+      })
+    }
+  }
+
   const { msg } = alerta;
 
   return (
@@ -46,30 +73,35 @@ const NuevoPassword = () => {
         />}
         
         { tokenValido && (
-          <form>
-            {/* Password */}
-            <div className="my-5">
-              <label
-                className="uppercase text-gray-600 block text-xl font-bold"
-              >
-                Nuevo Password
-              </label>
-              <input
-                type="password"
-                placeholder="Tu Nuevo Password"
-                className="border w-full p-3 mt-3 bg-gray-50 rounded-xl"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-              />
-              {/* submit */}
-              <input
-                type="submit"
-                value="Guardar Nuevo Password"
-                className="bg-indigo-700 w-full py-3 px-10 rounded-xl text-white uppercase font-bold mt-5 hover:cursor-pointer hover:bg-indigo-800 md:w-auto"
-              />
-            </div>
-          </form>
+          <>
+            <form onSubmit={handleSubmit}>
+              {/* Password */}
+              <div className="my-5">
+                <label
+                  className="uppercase text-gray-600 block text-xl font-bold"
+                >
+                  Nuevo Password
+                </label>
+                <input
+                  type="password"
+                  placeholder="Tu Nuevo Password"
+                  className="border w-full p-3 mt-3 bg-gray-50 rounded-xl"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+                {/* submit */}
+                <input
+                  type="submit"
+                  value="Guardar Nuevo Password"
+                  className="bg-indigo-700 w-full py-3 px-10 rounded-xl text-white uppercase font-bold mt-5 hover:cursor-pointer hover:bg-indigo-800 md:w-auto"
+                />
+              </div>
+            </form>
+          </>
         )}
+
+        { passwordModificado && <Link className="block text-center my-5 text-gray-500" to="/">Iniciar sesión</Link> }
+        
       </div>
     </>
   );
